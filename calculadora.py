a1e08d62fb0a273947a234830109553c1c1054e8
jblemos <jonhblvm@outlook.com>
2022-07-13 10:54:47 -0300

    style: adequacao ao pep8 do python

--- calculadora.py
+++ calculadora.py
@@ -9,13 +9,13 @@ def sum(a, b):
     Return:
         int
     """
-    return a+b
+    return a + b
 
 def sub(a, b):
-    return a-b
+    return a - b
 
 def div(a, b):
-    return a/b
+    return a / b
 
 def exp(a, b):
-    return a*b
+    return a * b
